376489dfcb61e15cfc96406d3c0491962b32b52d
zesantos-ctrl <jvitinhosa@gmail.com>
2025-01-09 11:56:33 -0300

    Add files via upload

---
+++ HTML_CSS_Completo/script.js
@@ -0,0 +1,14 @@
+function verificar() {
+    let nome = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let assunto = document.getElementById("assunto").value;
+    let comentario = document.getElementById("comentario").value;
+}
+
+if (nome.length < 2 && nome.trin() ==="") {
+    alert("")
+}
+
+else if ()
+
+
